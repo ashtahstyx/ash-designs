3b51fb618596717d0d7242e5173449d42d7ae292
Ashley Hunt <ashleyhunt@macbookpro.mynetworksettings.com>
2025-06-11 11:06:43 -0500

    ash-designs - update main styles, update home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/App.scss';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -9,23 +10,25 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Router
-      basename="/ash-designs"
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/gamehive" element={<GameHive />} />
-          <Route path="/garden-party" element={<GardenParty />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <div className="layout">
+      <Router
+        basename="/ash-designs"
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}>
+        <Header />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gamehive" element={<GameHive />} />
+            <Route path="/garden-party" element={<GardenParty />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,13 +5,6 @@ import { FaArrowRight } from 'react-icons/fa';
 const Home = () => {
   return (
     <div className={styles.home}>
-      {/* <div className={styles.homeIntro}>
-        <h2>Hi, I'm Ash</h2>
-        <span>
-          Your brand deserves a website that looks sharp and works flawlessly. I
-          design and build digital experiences that do both.
-        </span>
-      </div> */}
       <div className={styles.homeWrapper}>
         <div className={styles.title}>
           <div className={styles.titleTop}>
