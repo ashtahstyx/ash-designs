828059dee9a2fa52ed40add16f623e71131c0dd6
Ashley Hunt <ashleyhunt@MacBookPro.lan>
2025-06-17 16:05:34 -0500

    ash-designs - coo update variables, start loliware

--- src/pages/Loliware/Loliware.tsx
+++ src/pages/Loliware/Loliware.tsx
@@ -12,25 +12,30 @@ const Loliware = () => {
 
   return (
     <>
-      <Accordion className={styles.accordion}>
-        <AccordionItem
-          className={styles.accordionItem}
-          header={
-            <div className={styles.accordionHeader} onClick={handleToggle}>
-              <span>What is Game Hive?</span>
-              <FaChevronDown
-                className={`${styles.accordionHeaderIcon} ${
-                  isOpen ? styles.rotate : ''
-                }`}
-              />
-            </div>
-          }>
-          <div className={styles.accordionContent}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-        </AccordionItem>
-      </Accordion>
+      <main>
+        <section></section>
+        <section>
+          <Accordion className={styles.accordion}>
+            <AccordionItem
+              className={styles.accordionItem}
+              header={
+                <div className={styles.accordionHeader} onClick={handleToggle}>
+                  <span>What is Game Hive?</span>
+                  <FaChevronDown
+                    className={`${styles.accordionHeaderIcon} ${
+                      isOpen ? styles.rotate : ''
+                    }`}
+                  />
+                </div>
+              }>
+              <div className={styles.accordionContent}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </AccordionItem>
+          </Accordion>
+        </section>
+      </main>
     </>
   );
 };
